87babd749a2279a9cfd6570153c427d474464ae4
Gustavo <gustavo.tiago.gts@outlook.com>
2020-05-17 21:55:47 -0300

    inserção de pacientes na internação

--- src/br/com/fiap/cap/Main.java
+++ src/br/com/fiap/cap/Main.java
@@ -5,110 +5,112 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		int registro, opcao = 0;
 		String nome, cpf;
-		String [] perguntas = {"Está com tosse?", "Está com falta de ar?", "Está com febre?"};
-		String [] respostas = new String [3];
-		String [] leitos = new String [2];
-		
+		String[] perguntas = { "Estï¿½ com tosse?", "Estï¿½ com falta de ar?", "Estï¿½ com febre?" };
+		String[] respostas = new String[3];
+		String[] leitos = new String[2];
+
 		Fila atendimento = new Fila();
 		Fila internacao = new Fila();
-		
+
 		atendimento.init();
 		internacao.init();
-		
+
+		int topo = 0;
+
 		System.out.println("Bem vindo ao atendimento desta unidade!");
-		System.out.println("Selecione uma das opeções abaixo: ");
-		
-		while(opcao != 4) {
+		System.out.println("Selecione uma das opeï¿½ï¿½es abaixo: ");
+
+		while (opcao != 4) {
 			System.out.println("Digite 1 para cadastrar um paciente.");
 			System.out.println("Digite 2 para atender o paciente.");
 			System.out.println("Digite 3 para");
-			System.out.println("Digite 4 para finalizar o dia!");	
+			System.out.println("Digite 4 para finalizar o dia!");
 			opcao = sc.nextInt();
-			
-			Paciente p = null; 
-			
-			switch(opcao) {
+
+			Paciente p = null;
+
+			switch (opcao) {
 			case 1:
 				p = new Paciente();
-				
+
 				System.out.println("Digite o registro: ");
 				registro = sc.nextInt();
 				p.setRegistro(registro);
 				sc.nextLine();
-				
+
 				System.out.println("Digite o nome do paciente:");
 				nome = sc.nextLine();
 				p.setNome(nome);
-				
+
 				System.out.println("Digite o cpf do paciente:  ");
 				cpf = sc.nextLine();
 				p.setCpf(cpf);
-				
+
 				atendimento.enqueue(p);
-				
+
 				break;
-				
+
 			case 2:
-				if(!atendimento.isEmpty()) {
+				if (!atendimento.isEmpty()) {
 					Paciente atendido = atendimento.dequeue();
 					System.out.println("Retirado da lista: " + atendido.getNome());
-		
+
 					sc.nextLine();
-					
-					for (int i = 0; i< perguntas.length; i++) {
+
+					for (int i = 0; i < perguntas.length; i++) {
 						System.out.println(perguntas[i]);
 						respostas[i] = sc.nextLine().toUpperCase();
 					}
-					
+
 					for (int i = 0; i < respostas.length; i++) {
 						System.out.println(respostas[i]);
 					}
-					
+
 					if (respostas[0].equals("SIM") && respostas[1].equals("SIM") && respostas[2].equals("SIM")) {
 						for (int i = 0; i < leitos.length; i++) {
-							if(leitos[i] == null) {
-								leitos[i] = atendido.getCpf();
-							} else {
+							if (topo == leitos.length) {
 								atendimento.enqueue(atendido);
-								System.out.println("Adicionado na lista de internação.");
+								System.out.println("Adicionado na lista de internados.");
+								break;
+							} else if (leitos[topo] == null) {
+								leitos[topo] = atendido.getCpf();
+								topo++;
+								break;
 							}
 						}
 					}
-					
-					else if(respostas[1].equals("SIM")) {
+
+					else if (respostas[1].equals("SIM")) {
 						for (int i = 0; i < leitos.length; i++) {
-							if(leitos[i] == null) {
+							if (leitos[i] == null) {
 								leitos[i] = atendido.getCpf();
 							} else {
 								atendimento.enqueue(atendido);
 							}
 						}
+					} else {
+						System.out.println("Vocï¿½ foi liberado, fique bem!");
 					}
-					else {
-						System.out.println("Você foi liberado, fique bem!");
-					}	
-					
+
 					for (int i = 0; i < leitos.length; i++) {
 						System.out.println(leitos[i]);
 					}
 				}
-				
-				
+
 				break;
 			case 3:
-				//Logica liberacao internacao
+				// Logica liberacao internacao
 				break;
-			
+
 			default:
-				System.out.println("Até breve e fique bem!");
+				System.out.println("Atï¿½ breve e fique bem!");
 			}
 		}
 
-		
 		sc.close();
 	}
 
